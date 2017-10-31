675a1cebe8597b7228254b767b94fe99d9002a48
NikolajLJO <nljarr15@student.aau.dk>
2017-10-31 10:54:25 +0100

    Temp file splitting

--- Cube.h
+++ Cube.h
@@ -1,36 +1,30 @@
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
-#include "Graph.h"
 
-
-
-struct cube_t
-{
- char sides[6][9];
- /* top
+char myCube[6][10];
+char testCube[6][10];
+  /*
+  top
 	right
 	front
 	back
 	left
 	buttom
-*/
-};
+  */
 
-struct cube_t MyCube;
-struct cube_t Testcube1;
+
 
 void InitializeSide(char a, char b, char c, char d, char e, char f, char g, char h, char i, int sidenumber)
 {
-	MyCube.sides[sidenumber][0] = a;
-	MyCube.sides[sidenumber][1] = b;
-	MyCube.sides[sidenumber][2] = c;
-	MyCube.sides[sidenumber][3] = d;
-	MyCube.sides[sidenumber][4] = e;
-	MyCube.sides[sidenumber][5] = f;
-	MyCube.sides[sidenumber][6] = g;
-	MyCube.sides[sidenumber][7] = h;
-	MyCube.sides[sidenumber][8] = i;
+	myCube[sidenumber][0] = a;
+	myCube[sidenumber][1] = b;
+	myCube[sidenumber][2] = c;
+	myCube[sidenumber][3] = d;
+	myCube[sidenumber][4] = e;
+	myCube[sidenumber][6] = g;
+	myCube[sidenumber][5] = f;
+	myCube[sidenumber][7] = h;
+	myCube[sidenumber][8] = i;
 }
 
 void InitializeCube()/* init with hardcoded colors */
@@ -53,47 +47,53 @@ void InitializeCubeTest()
 	InitializeSide('r','r','r','y','b','b','o','o','y', 5);	/* init buttom */
 }
 
-void SwapOnFace(int FaceIdentifier, int SpaceOne, int SpaceTwo) /* swap space one with space two on a given face */
+void SwapOnFace(int faceIdentifier, int spaceOne, int spaceTwo) /* swap space one with space two on a given face */
 {
 	char temp;
-	temp = MyCube.sides[FaceIdentifier][SpaceTwo];
-	MyCube.sides[FaceIdentifier][SpaceTwo] = MyCube.sides[FaceIdentifier][SpaceOne];
-	MyCube.sides[FaceIdentifier][SpaceOne] = temp;
+	temp = myCube[faceIdentifier][spaceTwo];
+	myCube[faceIdentifier][spaceTwo] = myCube[faceIdentifier][spaceOne];
+	myCube[faceIdentifier][spaceOne] = temp;
 }
 
-void RotateFaceRight(int FaceIdentifier) /* rotate a face to the right (like when you rotate right side this is what happens to the colours on the right face) */
+void RotateFaceRight(int faceIdentifier) /* rotate a face to the right (like when you rotate right side this is what happens to the colours on the right face) */
 {
-	SwapOnFace(FaceIdentifier,1,3);
-	SwapOnFace(FaceIdentifier,3,7);
-	SwapOnFace(FaceIdentifier,7,5);
-	SwapOnFace(FaceIdentifier,0,6);
-	SwapOnFace(FaceIdentifier,6,8);
-	SwapOnFace(FaceIdentifier,8,2);
+	SwapOnFace(faceIdentifier,1,3);
+	SwapOnFace(faceIdentifier,3,7);
+	SwapOnFace(faceIdentifier,7,5);
+	SwapOnFace(faceIdentifier,0,6);
+	SwapOnFace(faceIdentifier,6,8);
+	SwapOnFace(faceIdentifier,8,2);
 }
 
 void SwapBetweenFaces(int FaceOne, int SpaceOne, int FaceTwo, int SpaceTwo) /* swap space one on face one with space two on face two */
 {
 	char temp;
-	temp = MyCube.sides[FaceTwo][SpaceTwo];
-	MyCube.sides[FaceTwo][SpaceTwo] = MyCube.sides[FaceOne][SpaceOne];
-	MyCube.sides[FaceOne][SpaceOne] = temp;
+	temp = myCube[FaceTwo][SpaceTwo];
+	myCube[FaceTwo][SpaceTwo] = myCube[FaceOne][SpaceOne];
+	myCube[FaceOne][SpaceOne] = temp;
 }
 
-void FaceSwap(int FaceOneIdentifier, int FaceTwoIdentifier) /* swap the pointer of two faces so they "swap places" */
+void FaceSwap(int faceOneIdentifier, int faceTwoIdentifier) /* swap the pointer of two faces so they "swap places" */
 {
-
-	char temp[9];
-	memcpy(temp, MyCube.sides[FaceTwoIdentifier], 9);
-	memcpy(MyCube.sides[FaceTwoIdentifier], MyCube.sides[FaceOneIdentifier], 9);
-	memcpy(MyCube.sides[FaceOneIdentifier], temp, 9);
+	char temp;
+	
+	for(int i = 0; i < 9; i++)
+  {
+    temp = myCube[faceTwoIdentifier][i];
+    myCube[faceTwoIdentifier][i] = myCube[faceOneIdentifier][i];
+    myCube[faceOneIdentifier][i] = temp;
+  }
+	// memcpy(temp, myCube[faceTwoIdentifier], 9);
+	//memcpy(myCube[faceTwoIdentifier], myCube[faceOneIdentifier], 9);
+	//memcpy(myCube[faceOneIdentifier], temp, 9);
 }
 
 void TurnCubeRight() /* turn cube without holding it, aka turning the cube entirly */
-{	
+{
 	/* face swap operations */
 	/* swap front to left */
 	FaceSwap(4,2);
-	
+
 	/*swap left to back */
 	SwapBetweenFaces(4,0,3,8);
 	SwapBetweenFaces(4,1,3,7);
@@ -104,29 +104,29 @@ void TurnCubeRight() /* turn cube without holding it, aka turning the cube entir
 	SwapBetweenFaces(4,6,3,2);
 	SwapBetweenFaces(4,7,3,1);
 	SwapBetweenFaces(4,8,3,0);
-	
+
 	/*swap back to left */
 	FaceSwap(3,1);
-	
+
 	/* face rotate operations */
 	/* top face operations */
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i<3 ; i++)
 		RotateFaceRight(0);
-	
+
 	/* bot face operations */
 	RotateFaceRight(5);
 }
 
 void RotateButtomRight() /*holding the top and rotating the buttom face */
-{	
+{
 	/* swap colors on bot */
 	RotateFaceRight(5);
-	
+
 	/* rotate the 4 rows that get rotated */
 	/* front 7,8,9 to left 7,8,9 */
 	SwapBetweenFaces(2,6,4,6);
 	SwapBetweenFaces(2,7,4,7);
-	SwapBetweenFaces(2,8,4,8);	
+	SwapBetweenFaces(2,8,4,8);
 	/* swap 7,8,9 left with 3,2,1 back */
 	SwapBetweenFaces(4,6,3,2);
 	SwapBetweenFaces(4,7,3,1);
@@ -135,7 +135,7 @@ void RotateButtomRight() /*holding the top and rotating the buttom face */
 	SwapBetweenFaces(3,2,1,2);
 	SwapBetweenFaces(3,1,1,1);
 	SwapBetweenFaces(3,0,1,0);
-	
+
 }
 
 void ClawPull() /* representation changes when the claw pulls the cube front to top etc. */
@@ -151,7 +151,7 @@ void ClawPull() /* representation changes when the claw pulls the cube front to 
 		RotateFaceRight(4);
 }
 
-/* 
+/*
 Compound functions are moves to be executed by hand like inverted right or top or inverted bottom
 but reflect this in the representation since this is multiple moves, or a compound of other functions ;-)
 */
@@ -167,7 +167,7 @@ void CompoundInvertedRight()
 {
 	TurnCubeRight();
 	ClawPull();
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			RotateButtomRight();
 		}
@@ -175,7 +175,7 @@ void CompoundInvertedRight()
 
 void CompoundLeft()
 {
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			TurnCubeRight();
 		}
@@ -185,12 +185,12 @@ void CompoundLeft()
 
 void CompoundInvertedLeft()
 {
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			TurnCubeRight();
 		}
 	ClawPull();
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			RotateButtomRight();
 		}
@@ -198,7 +198,7 @@ void CompoundInvertedLeft()
 
 void CompoundTop ()
 {
-	for(int i = 0; i<2;i++)
+	for(int i = 0; i < 2; i++)
 		{
 			ClawPull();
 		}
@@ -207,11 +207,11 @@ void CompoundTop ()
 
 void CompoundInvertedTop()
 {
-	for(int i = 0; i<2;i++)
+	for(int i = 0; i < 2; i++)
 		{
 			ClawPull();
 		}
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			RotateButtomRight();
 		}
@@ -224,7 +224,7 @@ void CompoundBottom()
 
 void CompoundInvertedBottom()
 {
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			RotateButtomRight();
 		}
@@ -232,7 +232,7 @@ void CompoundInvertedBottom()
 
 void CompoundFront()
 {
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			ClawPull();
 		}
@@ -241,11 +241,11 @@ void CompoundFront()
 
 void CompoundInvertedFront()
 {
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			ClawPull();
 		}
-	for(int i = 0; i<3;i++)
+	for(int i = 0; i < 3; i++)
 		{
 			RotateButtomRight();
 		}
@@ -270,15 +270,23 @@ void FillTestCube()
 {
 	for(int j = 0; j < 6; j++)
 	{
-		memcpy(Testcube1.sides[j], MyCube.sides[j], 9);
+    for(int i = 0; i < 9; i++)
+    {
+      testCube[j][i] = myCube[j][i];
+    }
+		//memcpy(Testcube1.sides[j], myCube[j], 9);
 	}
 }
 
-void resetcube()
+void Resetcube()
 {
-	for(int i = 0; i<6;i++)
+	for(int j = 0; j < 6; j++)
 	{
-		memcpy(MyCube.sides[i], Testcube1.sides[i], 9);
+    for(int i = 0; i < 9; i++)
+    {
+      myCube[j][i] = testCube[j][i];
+    }
+		//memcpy(myCube[i], Testcube1.sides[i], 9);
 	}
 }
 
@@ -287,109 +295,100 @@ void resetcube()
 void TestMethods()
 {
 	RotateButtomRight();
-	if(	strncmp(MyCube.sides[5],"oyrobrybr",9) == 0 /* COORECT */
-		&& strncmp(MyCube.sides[1], "wwwyowobo",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[2], "gywgwowog",9) == 0 /*CORRECT */
-		&& strncmp(MyCube.sides[3], "rwbgygybb",9) == 0) /* CORRECT*/
+	if(	strncmp(myCube[5],"oyrobrybr",9) == 0 /* COORECT */
+		&& strncmp(myCube[1], "wwwyowobo",9) == 0 /* CORRECT */
+		&& strncmp(myCube[2], "gywgwowog",9) == 0 /*CORRECT */
+		&& strncmp(myCube[3], "rwbgygybb",9) == 0) /* CORRECT*/
 		{
 			printf("Test 1 succes!\n");
 		}
-		
+
 	else
-	{
-		printf("Test 1 failed!\n");
+	{  
 			for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[5][i]);
+			printf("%c", myCube[5][i]);
 		printf("\n");
 				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[1][i]);
+			printf("%c", myCube[1][i]);
 		printf("\n");
 				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[2][i]);
+			printf("%c", myCube[2][i]);
 		printf("\n");
 		for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[3][i]);
+			printf("%c", myCube[3][i]);
 		printf("\n");
 	}
-		
-	resetcube();
-	
+
+	Resetcube();
+
 	ClawPull();
-	if(	strncmp(MyCube.sides[0],"gywgwowww",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[1], "oyrbowowb",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[2], "rrrybbooy",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[3], "ooyrgrybg",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[4], "rrgyrogww",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[5], "bgbgygybb",9) == 0) /* CORRECT */
+	if(	strncmp(myCube[0],"gywgwowww",9) == 0 /* CORRECT */
+		&& strncmp(myCube[1], "oyrbowowb",9) == 0 /* CORRECT */
+		&& strncmp(myCube[2], "rrrybbooy",9) == 0 /* CORRECT */
+		&& strncmp(myCube[3], "ooyrgrybg",9) == 0 /* CORRECT */
+		&& strncmp(myCube[4], "rrgyrogww",9) == 0 /* CORRECT */
+		&& strncmp(myCube[5], "bgbgygybb",9) == 0) /* CORRECT */
 		{
 			printf("Test 2 succes!\n");
 		}
 	else
-	{
-		printf("Test 2 failed!\n");
-				printf("Test 1 failed!\n");
+	{   
 			for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[0][i]);
+			printf("%c", myCube[0][i]);
 		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[1][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[2][i]);
-		printf("\n");
-		for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[3][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[4][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[5][i]);
-		printf("\n");
-	}
-		
-	resetcube();
-	
-	TurnCubeRight();
-	if(	strncmp(MyCube.sides[0],"yrgogbory",9) == 0 /* COORECT */
-		&& strncmp(MyCube.sides[1], "wwwowgwyg",9) == 0 /* COORECT */
-		&& strncmp(MyCube.sides[2], "gyrwrrwog",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[3], "rwbyowobo",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[4], "bbygygbgb",9) == 0 /* CORRECT */
-		&& strncmp(MyCube.sides[5], "oyrobrybr",9) == 0) /* COORECT */
-		printf("Test 3 succes!\n");
-	else
-	{
-		printf("Test 3 failed!\n");
-			for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[0][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[1][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[2][i]);
-		printf("\n");
-		for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[3][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[4][i]);
-		printf("\n");
-				for (int i = 0; i < 9; i++)
-			printf("%c", MyCube.sides[5][i]);
-		printf("\n");
-	}
-	
-	resetcube();
-}
 
-void main()/* main program starts here */
-{
-	InitializeCubeTest();
-	FillTestCube();
-	TestMethods();
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[1][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[2][i]);
+		printf("\n");
+		for (int i = 0; i < 9; i++)
+			printf("%c", myCube[3][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[4][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[5][i]);
+		printf("\n");
+	}
+
+	Resetcube();
+
+	TurnCubeRight();
+	if(	strncmp(myCube[0],"yrgogbory",9) == 0 /* COORECT */
+		&& strncmp(myCube[1], "wwwowgwyg",9) == 0 /* COORECT */
+		&& strncmp(myCube[2], "gyrwrrwog",9) == 0 /* CORRECT */
+		&& strncmp(myCube[3], "rwbyowobo",9) == 0 /* CORRECT */
+		&& strncmp(myCube[4], "bbygygbgb",9) == 0 /* CORRECT */
+		&& strncmp(myCube[5], "oyrobrybr",9) == 0) /* COORECT */
+		{
+			printf("Test 3 succes!\n");
+		}
+	else
+	{    
+
+			for (int i = 0; i < 9; i++)
+			printf("%c", myCube[0][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[1][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[2][i]);
+		printf("\n");
+		for (int i = 0; i < 9; i++)
+			printf("%c", myCube[3][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[4][i]);
+		printf("\n");
+				for (int i = 0; i < 9; i++)
+			printf("%c", myCube[5][i]);
+		printf("\n");
 	
-	GRAPH[][][1] = 'g';
-	
+	}
+
+	Resetcube();
 }
---
+++ TesterFil2.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "Graph.h"
+
+
+void main()
+{
+	InitializeCube();
+//	InitializeCubeTest();
+//	FillTestCube();
+//	TestMethods();
+//	Wait(5000);
+
+	CreateGraph();
+}
