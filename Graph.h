8027331a15918a80b97ca572294eaa8783cfdfc1
Randers114 <srande15@student.aau.dk>
2017-11-14 10:51:57 +0100

    The start of the graph/algoritm solution

--- Graph.h
+++ Graph.h
@@ -1,5 +1,95 @@
-#include "Cube.h"
-
 #define SIZEOFCUBE 54
-char graph[1][6][9];
+#include "Heuristic.h"
+
+struct Node 
+{
+	char cube[6][10];
+	int heuristicValue;
+	int parent;
+};
+
+//char graph[1][6][9];
+
+Node Graph[100];
+
+
+void ConstructNode(int currentPosition, int parent)
+{
+	Node node;
+	memcpy(myCube, node.cube, sizeof myCube);
+	node.parent = parent;
+	node.heuristicValue = HeuristicValue();
+	Graph[currentPosition] = node;
+}
+
+
+void ConstructNodeChildren(int &currentPosition, int &layer)
+{
+	int parent = currentPosition;
+	currentPosition++;
+	RightOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedRightOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	LeftOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedLeftOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	TopOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedTopOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	BottomOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedBottomOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	FrontOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedFrontOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	BackOperation();
+	ConstructNode(currentPosition, parent);
+	
+	currentPosition++;
+	InvertedBackOperation();
+	ConstructNode(currentPosition, parent);
+	
+	layer++;
+	currentPosition;
+}
+
+void MainGraphConstruction()
+{
+	int currentPosition = 0, layer = 0;
+	ConstructNode(currentPosition, -1);
+	ConstructNodeChildren(currentPosition, layer);
+	
+	
+	
+	
+	
+	
+}
+
+
 
