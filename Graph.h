3c09f0375cdc3e7e8b42b4d1b01a8dee12ef4eeb
Randers114 <srande15@student.aau.dk>
2017-11-15 15:41:13 +0100

    shit be changed yo

--- Graph.h
+++ Graph.h
@@ -1,5 +1,22 @@
-#define SIZEOFCUBE 54
 #include "Heuristic.h"
+
+#define BOUND 8
+#define HIGHEST_HEURISTIC_VALUE 12
+#define GRAPH_SIZE 120
+
+#define RIGHT 1
+#define INVERTEDRIGHT 2
+#define LEFT 3
+#define INVERTEDLEFT 4
+#define TOP 5
+#define INVERTEDTOP 6
+#define BOTTOM 7
+#define INVERTEDBOTTOM 8
+#define FRONT 9
+#define INVERTEDFRONT 10
+#define BACK 11
+#define INVERTEDBACK 12
+
 
 struct Node 
 {
@@ -9,9 +26,17 @@ struct Node
 	int move;
 };
 
-//char graph[1][6][9];
+struct Values
+{
+	int upperHeuristic;
+	int currentPosition; 
+	int layer;
+	int childChoose;
+	int currentArrayPosition;
+}
 
-Node Graph[100];
+Node Graph[GRAPH_SIZE];
+int Path[BOUND * HIGHEST_HEURISTIC_VALUE];
 
 
 void ConstructNode(int currentPosition, int parent)
@@ -25,107 +50,245 @@ void ConstructNode(int currentPosition, int parent)
 
 
 void ConstructNodeChildren(int &currentPosition, int &layer)
-{
+{	
 	int parent = currentPosition;
 	char rubix[6][10];
 	memcpy(myCube, rubix, sizeof myCube);
 	
-	currentPosition++;
-	RightOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 1;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedRightOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 2;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	LeftOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 3;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedLeftOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 4;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	TopOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 5;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedTopOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 6;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	BottomOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 7;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedBottomOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 8;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	FrontOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 9;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedFrontOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 10;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	BackOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 11;
-	memcpy(rubix, myCube, sizeof rubix);
-	
-	currentPosition++;
-	InvertedBackOperation();
-	ConstructNode(currentPosition, parent);
-	Graph[currentPosition].move = 12;
-	memcpy(rubix, myCube, sizeof rubix);
-	
+	for(int move = 1; move < 13; move++)
+	{
+		currentPosition++;
+		switch(move)
+		{
+			case RIGHT:
+				RightOperation();
+				break;
+			case INVERTEDRIGHT:
+				InvertedRightOperation();
+				break;
+			case LEFT:
+				LeftOperation();
+				break;
+			case INVERTEDLEFT:
+				InvertedLeftOperation();
+				break;
+			case TOP:
+				TopOperation();
+				break;
+			case INVERTEDTOP:
+				InvertedTopOperation();
+				break;
+			case BOTTOM:
+				BottomOperation();
+				break;
+			case INVERTEDBOTTOM:
+				InvertedBottomOperation();
+				break;
+			case FRONT:
+				FrontOperation();
+				break;
+			case INVERTEDFRONT:
+				InvertedFrontOperation();
+				break;
+			case BACK:
+				BackOperation();
+				break;
+			case INVERTEDBACK:
+				InvertedBackOperation();
+				break;
+		}
+		ConstructNode(currentPosition, parent);
+		Graph[currentPosition].move = move;
+		memcpy(rubix, myCube, sizeof rubix);
+	}	
 	layer++;
 }
 
-void MainGraphConstruction()
+int SaveThePath(int currentArrayPosition, int currentPosition, int layer)
 {
-	int currentPosition = 0, layer = 0, bound = 8, childChoose = 0;
-	ConstructNode(currentPosition, -1);
-	ConstructNodeChildren(currentPosition, layer);
+	int tempPath[BOUND];
 	
-	for(layer; layer <= bound;)
+	// Temp array is to reverse the moveset in the path array
+	for(int i = 0; i < layer; i++)
 	{
-		Node newParent = Graph[childChoose];
+		tempPath[i] = Graph[currentPosition].move;
 		
-		childChoose = layer * (12 - newParent.move) + 1;
-		
-		memcpy(newParent.cube, myCube, sizeof newParent.cube);
-		ConstructNodeChildren(currentPosition, layer);
-		
+		currentPosition = Graph[currentPosition].parent;
 		
 	}
 	
 	
+	// Temp array is assigned to path
+	for(int i = layer; i > 0; i--)
+	{
+		Path[currentArrayPosition] = tempPath[i];
+		
+		currentArrayPosition++;
+	}
+}
+
+void ResetValues(int &upperHeuristic, int &currentPosition, int &layer, int &childChoose)
+{
+	upperHeuristic = Graph[currentPosition].heuristicValue;
+	currentPosition = 0;
+	layer = 0;
+	childChoose = 0;
+}
+
+void PrepareNewTree(int &upperHeuristic, int &currentPosition, int &layer, int &childChoose, int &currentArrayPosition)
+{
+	Node newRoot = Graph[currentPosition];
+	currentArrayPosition = SaveThePath(currentArrayPosition, currentPosition, layer);
+	
+	//Resets all values to initial 
+	ResetValues(upperHeuristic, currentPosition, layer, childChoose);
+	
+	for(int index = 0; index < GRAPH_SIZE; index++)
+	{
+		Graph[index] = NULL;
+	}
+	
+	//Appoints a new root for the new tree
+	Graph[currentPosition] = newRoot;
+}
+
+bool CheckBottomLayerLeaves(int &upperHeuristic, int &currentPosition, int &layer, int &childChoose, int &currentArrayPosition)
+{
+	bool lastLeaf = false;
+	
+	// Set current position to leftmost lowest leaf
+	currentPosition = childChoose;
+	
+	int previousPosition = currentPosition;
+	
+	for(int leaf = 0; leaf < 12; leaf++)
+	{
+		if(Graph[currentPosition + leaf].heuristicValue < upperHeuristic)
+		{
+			currentPosition = currentPosition + leaf;
+			
+			PrepareNewTree(upperHeuristic, currentPosition, layer, childChoose, currentArrayPosition);
+			
+			break;
+			
+		} else
+		{
+			if(leaf == 11)
+			{
+				currentPosition = Graph[currentPosition + leaf].parent;
+				lastLeaf = true;
+			} 
+			
+			//Delete the node
+			Graph[previousPosition + leaf] = NULL;
+			
+		}
+	}
+	
+	return lastLeaf;
+}
+
+bool CheckHeuristic(int &upperHeuristic, int &currentPosition, int &layer, int &childChoose, int &currentArrayPosition)
+{
+	bool heuristicIsLower = false;
+	if(Graph[currentPosition].heuristicValue < upperHeuristic)
+	{
+		PrepareNewTree(upperHeuristic, currentPosition, layer, childChoose, currentArrayPosition);
+		
+		heuristicIsLower = true;
+	}
+	
+	return heuristicIsLower;
+}
+
+bool MoveUpInLayers(int &upperHeuristic, int &currentPosition, int &layer, int &childChoose, int &currentArrayPosition)
+{
+	bool movingUp = false;
+	Node tempNode;
+	
+	if(CheckHeuristic(upperHeuristic, currentPosition, layer, childChoose, currentArrayPosition))
+	{
+		//do punk ass shit
+	} else
+	{
+		if(Graph[currentPosition].move != 12)
+		{
+			Graph[currentPosition] = NULL;
+			currentPosition++;
+			
+			// Refactor array
+			for(int i = currentPosition; i < currentPosition + (12 - Graph[currentPosition].move); i++)
+			{
+				tempNode = Graph[i];
+				
+				Graph[i - 1] = tempNode;
+				
+				Graph[i] = NULL;
+			}
+		} else 
+		{
+			movingUp = true;
+			tempPosition = currentPosition;
+			
+			currentPosition = Graph[currentPosition].parent;
+			
+			Graph[tempPosition] = NULL;			
+		}
+	}
+	
+	return movingUp;
+}
+
+void MainGraphConstruction()
+{
+	int currentPosition = 0, layer = 0, childChoose = 0, upperHeuristic = HIGHEST_HEURISTIC_VALUE, currentArrayPosition = 0;
+	
+	bool lastLeafReached; 
+	
+	ConstructNode(currentPosition, -1);
+	ConstructNodeChildren(currentPosition, layer);
+	
+	upperHeuristic = Graph[0].heuristicValue;
+
+	while(upperHeuristic != 0)
+	{
+		for(layer; layer < BOUND;)
+		{
+			Node newParent = Graph[childChoose];
+		
+			childChoose = layer * (12 - newParent.move) + 1;
+		
+			memcpy(newParent.cube, myCube, sizeof newParent.cube);
+			ConstructNodeChildren(currentPosition, layer);
+		}
+		
+		lastLeafReached = CheckBottomLayerLeaves(upperHeuristic, currentPosition, layer, childChoose, currentArrayPosition);
+		
+		
+		if(lastLeafReached)
+		{
+			bool movingUp = true;
+			
+			while(movingUp)
+			{
+				movingUp = MoveUpInLayers(upperHeuristic, currentPosition, layer, childChoose, currentArrayPosition);
+				layer--;
+			}
+			
+			//delete the parent node
+			//refactor array
+		}
+	}
+	
+		
+		
+		
 	
 	
+	
+	// Problems
+	// How to move several layers up at a time (possibly solved) (loop in loop maybe impossible)
+	// How to refactor the array (use - move) 
 	
 	
 }
