e1dc1b72c5a88de2e6fe3c6caa0ba5b28f296a2e
Randers114 <srande15@student.aau.dk>
2017-12-11 17:26:20 +0100

    memory analysis started

---
+++ Memory analysis/Graph.h
@@ -0,0 +1,406 @@
+#include "DeterministicYellowFace.h"
+
+#define BOUND 3
+#define HIGHEST_HEURISTIC_VALUE 12
+#define GRAPH_SIZE 10
+#define WORST_NUMBER_OF_CASES 4 
+#define NUMBER_OF_MOVES_IN_CASE 7
+
+#define RIGHT 1
+#define INVERTEDRIGHT 2
+#define LEFT 3
+#define INVERTEDLEFT 4
+#define TOP 5
+#define INVERTEDTOP 6
+#define BOTTOM 7
+#define INVERTEDBOTTOM 8
+#define FRONT 9
+#define INVERTEDFRONT 10
+#define BACK 11
+#define INVERTEDBACK 12
+
+
+struct Node 
+{
+	int heuristicValue;
+	int move;
+	char cube[6][10];
+};
+
+struct Values
+{
+	int upperHeuristic;
+	int currentPosition; 
+	int layer;
+	int currentArrayPosition;
+};
+
+Node Blank;
+Node TempRootNode;
+Node Graph[GRAPH_SIZE];
+
+int Bound;
+int Flag;	
+int TempPath[GRAPH_SIZE];
+int Path[HIGHEST_HEURISTIC_VALUE * GRAPH_SIZE + WORST_NUMBER_OF_CASES * NUMBER_OF_MOVES_IN_CASE];
+int ChildNumber[GRAPH_SIZE];
+int nodeCount = 0;
+
+void ConstructNode(int currentPosition, int move, int upperHeuristic)
+{
+	nodeCount++;
+	
+	currentPosition++;
+
+	Node node;
+	
+	memcpy(node.cube, myCube, sizeof myCube);
+	
+	if(upperHeuristic > 8)
+	{
+		node.heuristicValue = HeuristicValueWhiteCross();
+		upperHeuristic = node.heuristicValue;
+	}
+	if(upperHeuristic > 4 && upperHeuristic <= 8)
+	{
+		node.heuristicValue = HeuristicValueWhiteFace();
+		upperHeuristic = node.heuristicValue;
+	} 
+	if(upperHeuristic <= 4) 
+	{
+		node.heuristicValue = HeuristicValueCenterLayer();
+	}
+	
+	node.move = move;
+	
+	Graph[currentPosition] = node;
+}
+
+
+void ConstructNodeChildren(Values &values)
+{	
+	int move;
+	
+	for(values.layer; values.layer < Bound; ++values.currentPosition)
+	{
+		// ClearScreen();
+		// TextOut(0, LCD_LINE1, "Hey ");
+		// NumOut(0, LCD_LINE2, values.currentPosition);
+		//Wait(500);
+		
+		
+		Node newParent = Graph[values.currentPosition];
+		
+		memcpy(myCube, newParent.cube, sizeof newParent.cube);
+		
+		// If childnumber hits 12 resets the value to zero
+		if(ChildNumber[values.layer] == 12)
+		{
+			ChildNumber[values.layer] = 0;
+		}
+		
+		ChildNumber[values.layer] += 1;
+		
+		// // ChildNumber decides the move
+		move = ChildNumber[values.layer];
+		
+		// // Choose the operation for the node from childnumber
+		switch(move)
+		{
+			case RIGHT:
+				RightOperation();
+				break;
+			case INVERTEDRIGHT:
+				InvertedRightOperation();
+				break;
+			case LEFT:
+				LeftOperation();
+				break;
+			case INVERTEDLEFT:
+				InvertedLeftOperation();
+				break;
+			case TOP:
+				TopOperation();
+				break;
+			case INVERTEDTOP:
+				InvertedTopOperation();
+				break;
+			case BOTTOM:
+				BottomOperation();
+				break;
+			case INVERTEDBOTTOM:
+				InvertedBottomOperation();
+				break;
+			case FRONT:
+				FrontOperation();
+				break;
+			case INVERTEDFRONT:
+				InvertedFrontOperation();
+				break;
+			case BACK:
+				BackOperation();
+				break;
+			case INVERTEDBACK:
+				InvertedBackOperation();
+				break;
+			default:
+				break;
+		}
+		
+		ConstructNode(values.currentPosition, move, values.upperHeuristic);
+		
+		++values.layer;
+		
+		memcpy(myCube, newParent.cube, sizeof newParent.cube);
+	}
+}
+
+int SaveThePath(int currentArrayPosition, int currentPosition, int layer)
+{
+	for(int i = 0; i <= GRAPH_SIZE; i++)
+	{
+		if(Graph[i].move != 0)
+		{	
+			Path[currentArrayPosition] = Graph[i].move;
+		
+			currentArrayPosition++;
+		}
+	}
+	
+	return currentArrayPosition;
+}
+
+void ResetValues(Values &values)
+{
+	values.upperHeuristic = Graph[values.currentPosition].heuristicValue;
+	values.currentPosition = 0;
+	values.layer = 0;
+	TempRootNode.move = 0;
+	//nodeCount = 0;
+}
+
+void PrepareNewTree(Values &values)
+{
+	if(Flag)
+	{
+		//printf("Heuristic %d \n", TempRootNode.heuristicValue);
+		// TextOut(0, LCD_LINE1, "Heuristic value11 ");
+		// NumOut(0, LCD_LINE2, TempRootNode.heuristicValue);
+		// Wait(1000);
+		
+		for(int i = 0; i < GRAPH_SIZE; i++)
+		{
+			if(TempPath[i] != 0)
+			{
+				Path[values.currentArrayPosition] = TempPath[i];
+			
+				++values.currentArrayPosition;
+			}
+		}
+		
+		//Resets all values to initial 
+		values.upperHeuristic = TempRootNode.heuristicValue;
+		values.currentPosition = 0;
+		values.layer = 0;
+		TempRootNode.move = 0;
+		Flag = 0;
+		
+		for(int index = 0; index <= GRAPH_SIZE; index++)
+		{
+			Graph[index] = Blank;
+		}
+		
+		Graph[values.currentPosition] = TempRootNode;
+		
+		
+	} else
+	{
+		TempRootNode = Graph[values.currentPosition];
+	
+		// TextOut(0, LCD_LINE1, "Heuristic value22 ");
+		// NumOut(0, LCD_LINE2, TempRootNode.heuristicValue);
+		// Wait(1000);
+		//printf("Heuristic %d \n", TempRootNode.heuristicValue);
+		
+		values.currentArrayPosition = SaveThePath(values.currentArrayPosition, values.currentPosition, values.layer);
+		
+
+		//Resets all values to initial 
+		ResetValues(values);
+
+		for(int index = 0; index <= GRAPH_SIZE; index++)
+		{
+			Graph[index] = Blank;
+		}
+
+		//Appoints a new root for the new tree
+		Graph[values.currentPosition] = TempRootNode;
+	}
+
+}
+
+int CheckHeuristic(Values &values)
+{
+	int heuristicIsLower = 0;
+	
+	if(Graph[values.currentPosition].heuristicValue < values.upperHeuristic || (Graph[values.currentPosition].move == 12 && values.layer == 1))
+	{	
+		if(values.upperHeuristic > 4 && !(Graph[values.currentPosition].move == 12 && values.layer == 1))
+		{
+			Flag = 1;
+			
+			// ClearScreen();
+			// TextOut(0, LCD_LINE1, "hhhhhh");
+			// NumOut(0, LCD_LINE2, values.upperHeuristic);
+			// NumOut(0, LCD_LINE3, Graph[values.currentPosition].heuristicValue);
+			
+			
+			TempRootNode = Graph[values.currentPosition];
+			
+			for(int i = 0; i < GRAPH_SIZE; i++)
+			{
+				TempPath[i] = 0;
+			}
+			
+			for(int i = 0; i < GRAPH_SIZE; i++)
+			{
+				if(Graph[i].move != 0)
+				{
+					TempPath[i] = Graph[i].move;
+				}
+			}		
+			if(TempRootNode.heuristicValue != 0)
+			{
+				values.upperHeuristic = TempRootNode.heuristicValue;
+			} else 
+			{
+				values.upperHeuristic = TempRootNode.heuristicValue;
+				PrepareNewTree(values);
+				heuristicIsLower = 1;
+			}
+		} else if(Flag || Graph[values.currentPosition].heuristicValue < values.upperHeuristic)
+		{
+			PrepareNewTree(values);
+		
+			heuristicIsLower = 1;
+			
+			Bound = BOUND;
+
+		} else 
+		{
+			if(Bound >= 9)
+			{
+				//printf("Solution not reachable in appropriate time.. \n");
+				//exit(0);
+			} else 
+			{
+				Bound++;
+				values.currentPosition = 1;
+				values.layer = 0;
+				
+				for(int index = 1; index < GRAPH_SIZE; index++)
+				{
+					Graph[index] = Blank;
+				}
+			}
+		}
+		
+		
+	}
+	
+	return heuristicIsLower;
+}
+
+int CheckBottomLayerLeaves(Values &values)
+{
+	int lastLeaf = 0;
+
+	if(!CheckHeuristic(values))
+	{
+		lastLeaf = 1;
+	}
+	return lastLeaf;
+}
+
+
+
+int MoveUpInLayers(Values &values)
+{
+	int movingUp = 0;
+	int tempPosition;
+
+	if(!CheckHeuristic(values))
+	{
+
+		if(Graph[values.currentPosition].move == 12)
+		{
+			movingUp = 1;
+		}
+		
+		tempPosition = values.currentPosition;
+		
+		if(!values.currentPosition <= 0)
+		{
+			--values.currentPosition;
+		}
+		
+
+			
+		Graph[tempPosition] = Blank;
+	}
+	
+	return movingUp;
+}
+
+void MainGraphConstruction()
+{
+	int movingUp, lastLeafReached;
+	
+	Values values;
+	
+	values.upperHeuristic = HIGHEST_HEURISTIC_VALUE;
+	values.currentPosition = 0;
+	values.layer = 0;
+	values.currentArrayPosition = 0;
+	
+	
+	Flag = 0;
+	
+	ConstructNode(values.currentPosition - 1 , 0, HIGHEST_HEURISTIC_VALUE);
+	
+	values.upperHeuristic = Graph[0].heuristicValue;
+	
+	TextOut(0, LCD_LINE1, "Start Heuristic value ");
+	NumOut(0, LCD_LINE2, values.upperHeuristic);
+	Wait(1000);
+	ClearScreen();
+	
+	// printf("Start Heuristic value %d\n", values.upperHeuristic);
+	
+	
+	Bound = BOUND;
+		
+	while(values.upperHeuristic != 0)
+	{
+		ConstructNodeChildren(values);
+		
+		movingUp = CheckBottomLayerLeaves(values);
+		
+		while(movingUp)
+		{
+			movingUp = MoveUpInLayers(values);
+			values.layer--;
+			
+			if(values.layer < 0)
+			{
+				values.layer = 0;
+			}
+		}
+	}	
+	ClearScreen();
+	TextOut(0, LCD_LINE1, "Done ");
+	Wait(1000);
+}
+
+
+
